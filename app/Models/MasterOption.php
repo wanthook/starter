379e40d6012738e83e8c5aa314b1f0dd5e65865f
wanthook <taufiq@indahjaya.co.id>
2021-11-17 10:18:11 +0700

    Perubahan Master

--- app/Models/MasterOption.php
+++ app/Models/MasterOption.php
@@ -4,8 +4,88 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use DateTimeInterface;
 
 class MasterOption extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    
+    protected $table = 'master_options';
+    
+    protected $fillable = [
+        'kode',
+        'deskripsi',   
+        'tipe',
+        'warna',
+        'nilai',
+        'deleted_at',
+        'created_by', 
+        'created_at',
+        'updated_by', 
+        'updated_at'
+    ];
+    
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+    
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+    
+    public function getCreatedAtAttribute($value)
+    {
+        if(empty($value) || is_null($value))
+        {
+            return "";
+        }
+        return Carbon::parse($value)->format('d-m-Y H:i:s');
+    }
+    
+    public function formCreatedAtAttribute($value)
+    {
+        if(empty($value) || is_null($value))
+        {
+            return "";
+        }
+        
+        return Carbon::parse($value)->format('d-m-Y H:i:s');
+    }
+    
+    public function getUpdatedAtAttribute($value)
+    {
+        if(empty($value) || is_null($value))
+        {
+            return "";
+        }
+        return Carbon::parse($value)->format('d-m-Y H:i:s');
+    }
+    
+    public function formUpdatedAtAttribute($value)
+    {
+        if(empty($value) || is_null($value))
+        {
+            return "";
+        }
+        
+        return Carbon::parse($value)->format('d-m-Y H:i:s');
+    }
+    
+    public function createdBy()
+    {
+        return $this->belongsTo('App\User','created_by');
+    }
+    
+    public function updatedBy()
+    {
+        return $this->belongsTo('App\User','updated_by');
+    }
 }
